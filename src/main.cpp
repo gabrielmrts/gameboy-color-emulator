560112470aae555a36135fbf6d9b6e70d1635899
Gabriel <gabrielmrtss@gmail.com>
2025-09-19 21:32:34 -0300

    feat: add simple window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <SDL3/SDL.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+    if(!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("GameBoyEmu", 800, 600, SDL_WINDOW_RESIZABLE);
+
+    if (window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    bool done = false;
+    while (!done) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_QuitSubSystem(SDL_INIT_VIDEO);
+    SDL_Quit();
+
     return 0;
 }
